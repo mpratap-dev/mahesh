92254b4efe4560650fc144ee003d424e38358bcb
mpratap-dev <mp1995singh@gmail.com>
2020-11-29 18:57:06 +0530

    home page ui update

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ const classes = {
   button:
     "w-full flex items-center justify-center px-8 py-2 border border-transparent text-base rounded-lg text-white bg-indigo-800 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-300 ease-in-out md:py-2 md:text-md md:px-5",
   imageContainer:
-    "border-solid rounded-full mb-2 row-span-2 border-4 md:border-4 border-gray-200 md:mr-20 order-1 w-32 h-32 justify-self-center md:w-64 md:h-64 md:col-span-1 md:self-center md:justify-self-end md:order-2 md:mb-0 dark:border-gray-800",
+    "border-solid rounded-full mb-2 row-span-2 border-4 md:border-4 border-gray-200 md:border-white md:mr-20 order-1 w-32 h-32 justify-self-center md:w-64 md:h-64 md:col-span-1 md:self-center md:justify-self-end md:order-2 md:mb-0 dark:border-gray-800",
 };
 
 export default function Home() {
