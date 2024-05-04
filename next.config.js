30cacedf32ae16d42f2a215add1561c339cac002
Mahesh Pratap <maheshpratap@Maheshs-MacBook-Air.local>
2024-05-04 13:40:31 +0530

    Dev.to images fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     images: {
         domains: ['res.cloudinary.com'],
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'media.dev.to',
+              port: '',
+              pathname: '/cdn-cgi/image/**',
+            },
+          ],
     },
 }
