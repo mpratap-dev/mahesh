0302c0e13c13d096468e82ff3b95b25efd7fb874
Mahesh Pratap <maheshpratap@Maheshs-MacBook-Air.local>
2024-07-23 22:33:44 +0530

    Performance fixes

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,9 @@ const App = ({ Component, pageProps }) => (
     <Head>
       <title> Mahesh Pratap | UI / UX & Frontend Developer </title>{" "}
       <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800;900&display=swap" rel="stylesheet"/>
     </Head>
     <Nav />
     <div
