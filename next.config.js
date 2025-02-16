9c13bcb6d239eed1fae804306b783f912487f188
Mahesh Pratap <maheshpratap@Maheshs-MacBook-Air.local>
2025-02-16 19:39:43 +0530

    image host updated

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 module.exports = {
-    images: {
-        domains: ['media2.dev.to'],
-        remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'media2.dev.to',
-              port: '',
-              pathname: '/dynamic/image/**',
-            },
-          ],
-    },
+  images: {
+    domains: ["media2.dev.to"],
+  },
 }
